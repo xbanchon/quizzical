b030c85247481bacc3d61ce8efec3fe6864d03b3
Xavier Banchon <xbanchon@espol.edu.ec>
2024-01-06 01:52:19 -0500

    Fixed styles, added description in IntroPage component and a favicon

--- src/IntroPage.jsx
+++ src/IntroPage.jsx
@@ -4,7 +4,7 @@ export default function IntroPage(props) {
     <main className="intro--content">
       <h1 className="intro--title">Quizzical</h1>
       <p className="intro--description">
-        Some description lmao
+        Unleash your inner genius with Quizzical! 🧠 Test your knowledge, challenge friends, and discover fascinating facts across a multitude of categories. From pop culture to science, ignite your curiosity and compete for the trivia crown. Ready to become the ultimate trivia master?
       </p>
       <button className="intro--button" onClick={props.handleStart}>Start quiz</button>
     </main>
